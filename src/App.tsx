7eb9b7041241173ed14281eed1dba2e65cca8d88
Alfitrm <grackrev@gmail.com>
2026-07-03 15:07:00 +0700

    refactor: remove top status chrome from camera layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useRef, useState } from 'react'
 import { CameraControls } from './components/CameraControls'
 import { CameraView } from './components/CameraView'
-import { GestureStatus } from './components/GestureStatus'
-import { PhotoPreview } from './components/PhotoPreview'
 import { useCamera } from './hooks/useCamera'
 import { useGestureDetection } from './hooks/useGestureDetection'
 import { useStablePeace } from './hooks/useStablePeace'
@@ -47,14 +45,6 @@ function App() {
   return (
     <main className="min-h-screen bg-[#f5f6fa] px-4 py-5 text-[#49516a] sm:px-6">
       <div className="mx-auto flex min-h-screen max-w-5xl flex-col items-center justify-center gap-4">
-        <div className="flex w-full max-w-4xl items-center justify-between px-1">
-          <GestureStatus
-            gestureName={gesture.gestureName}
-            isPeaceDetected={gesture.isPeaceDetected}
-          />
-          <PhotoPreview photoDataUrl={photoDataUrl} />
-        </div>
-
         <CameraView
           captureDisabled={
             photoDataUrl !== null ||
